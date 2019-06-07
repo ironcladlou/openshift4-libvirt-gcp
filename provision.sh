ee087a490a058cde4248b0c9630b4d831e967fb4
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2019-06-07 11:48:34 +0530

    RHEL-8 VM config update for GCP nested virt

--- provision.sh
+++ provision.sh
@@ -6,32 +6,15 @@ set -o pipefail
 # Install tools
 sudo mv /tmp/tools/* /usr/local/bin
 
+# Grow the filesystem
+sudo growpart /dev/sda 1
+sudo xfs_growfs /
+
 # :-)
 sudo setenforce 0
 sudo sed -i -z 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 
-# Enable the latest libvirt packages
-sudo bash -c 'cat > /etc/yum.repos.d/sig-virt.repo' << EOF
-[sig-virt-libvirt-latest]
-name=SIG-Virt libvirt packages for CentOS 7 x86_64
-baseurl=http://mirror.centos.org/centos-7/7/virt/x86_64/libvirt-latest
-enabled=1
-EOF
-
-# Enable the latest QEMU packages
-sudo bash -c 'cat > /etc/yum.repos.d/sig-virt-kvm-common.repo' << EOF
-[sig-virt-kvm-common]
-name=SIG-Virt libvirt packages for CentOS 7 x86_64
-baseurl=http://mirror.centos.org/centos-7/7/virt/x86_64/kvm-common
-enabled=1
-EOF
-
-# Enable the latest Go packages
-sudo rpm --import https://mirror.go-repo.io/centos/RPM-GPG-KEY-GO-REPO
-curl -s https://mirror.go-repo.io/centos/go-repo.repo | sudo tee /etc/yum.repos.d/go-repo.repo
-
-# TODO: find the GPG key for SIG-Virt stuff
-sudo yum install -y --nogpg libvirt libvirt-devel libvirt-client git golang libvirt-daemon-kvm qemu-kvm bind-utils jq
+sudo yum install -y libvirt libvirt-devel libvirt-client git golang libvirt-daemon-kvm qemu-kvm bind-utils jq
 
 # Install yq to manipulate manifest file created by installer.
 if [[ ! -e /usr/local/bin/yq ]]; then
@@ -78,8 +61,7 @@ sudo systemctl restart libvirtd
 # Set up iptables and firewalld
 # TODO: discover the ports
 sudo iptables -I INPUT -p tcp -s 192.168.122.0/24 -d 192.168.122.1 --dport 16509 -j ACCEPT -m comment --comment "Allow insecure libvirt clients"
-sudo firewall-cmd --zone=trusted --add-source=192.168.126.0/24
-sudo firewall-cmd --zone=trusted --add-port=16509/tcp
+sudo firewall-cmd --zone=libvirt --add-service=libvirt --permanent
 
 # Enable NetworkManager DNS overlay
 # https://github.com/openshift/installer/blob/master/docs/dev/libvirt-howto.md#set-up-networkmanager-dns-overlay
@@ -107,9 +89,9 @@ update-rhcos-image
 
 echo "Installing oc client"
 cd $HOME
-curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux-4.1.0-rc.1.tar.gz
-tar -zxf openshift-client-linux-4.1.0-rc.1.tar.gz
-rm -fr openshift-client-linux-4.1.0-rc.1.tar.gz
+curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.1.0/openshift-client-linux-4.1.0.tar.gz
+tar -zxf openshift-client-linux-4.1.0.tar.gz
+rm -fr openshift-client-linux-4.1.0.tar.gz
 sudo mv $HOME/oc /usr/local/bin
 
 echo "Installing kubectl binary"
